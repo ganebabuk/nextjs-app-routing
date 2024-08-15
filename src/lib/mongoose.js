febe9b462e4373b00c35aca17302f4c5a8ca7a8f
ganebabuk <kganeshbabu.it@gmail.com>
2024-08-15 21:18:26 +0530

    changes

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose';
 async function connectToDatabase() {
-    const MONGODB_URI = process?.env?.MONGODB_URI;
-    if (!MONGODB_URI) {
-        throw new Error("Please define the MONGODB_URI environment variable inside .env");
-    }
     try {
+        const MONGODB_URI = process?.env?.MONGODB_URI;
         await mongoose.connect(MONGODB_URI);
         console.log("Connected to the mongoDB atlas cloud.");
     }
